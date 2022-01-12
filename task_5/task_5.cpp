342510c0efe8106bc974ef72ac8cf28eabede8cc
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-12 16:55:59 +0200

    5 задание готово

--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -33,66 +33,31 @@ int main()
 	int number{ 0 };
 	cin >> number;
 
-	int left{ 0 }, right{ 100 };
+	int left{ 0 }, right{ 100 }, iteration{ 0 };
 	int N = right;
 
 	do
 	{
-		N /= 2;
+		N = (left + right) / 2;
 
 		cout << "\n1. Ваше число < " << N
-			<< "\n2. Ваше число ==  " << N
+			<< "\n2. Ваше число == " << N
 			<< "\n3. Ваше число > " << N << endl;
 		int user_choice{ 0 };
 		cin >> user_choice;
 
 		if (user_choice == less_than_N)
-		{
 			right = N - 1;
-			break;
-		}
 		else if (user_choice == same_with_N)
-		{
-
-		}
-		else if (user_choice == bigger_than_N)
-		{
-			left = N + 1;
 			break;
-		}
+		else if (user_choice == bigger_than_N)
+			left = N + 1;
 
-
+		iteration++;
 
 	} while (number != N);
 
-	////int temp = N;
-	//do
-	//{
-	//	N /= 2;
-	//	cout << "\n1. Ваше число > " << N
-	//		<< "\n2. Ваше число < " << N
-	//		<< "\n3. Ваше число ==  " << N << endl;
-	//	int user_choice{ 0 };
-	//	cin >> user_choice;
-
-	//	switch (user_choice)
-	//	{
-	//	case Your_number_bigger_than_N:
-	//		left = N + 1;
-	//		N = (right-left)/2+left;
-	//		break;
-
-	//	case Your_number_less_than_N:
-	//		right = N - 1;
-	//		break;
-
-	//	case Your_number_same_with_N:
-	//		if (number == N)
-	//			cout << "Ваше число = " << N;
-	//		break;
-	//	}
-	//	//N = temp;
-	//} while (number != N);
+	cout << "\nВаше число " << N << " угадано с " << iteration + 1 << " попытки." << endl;
 
 	return 0;
 }
